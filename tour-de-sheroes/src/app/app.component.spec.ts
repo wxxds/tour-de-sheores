d62082e6a29e4b6d396173d6d98cd854a1a17e8d
Andrew Chase <andrew.chase@accenturefederal.com>
2020-07-29 13:28:40 -0400

    added comments component

--- tour-de-sheroes/src/app/app.component.spec.ts
+++ tour-de-sheroes/src/app/app.component.spec.ts
@@ -16,16 +16,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tour-de-sheroes'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('tour-de-sheroes');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('tour-de-sheroes app is running!');
-  });
 });
--- tour-de-sheroes/src/app/app.component.ts
+++ tour-de-sheroes/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {CommentNode} from './comments/comment-tree.component'
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent {
-  title = 'tour-de-sheroes';
+  comments:Array<CommentNode> = [];
+  constructor(){    
+    this.comments =  [new CommentNode("First")]
+  }
 }
+
 
 

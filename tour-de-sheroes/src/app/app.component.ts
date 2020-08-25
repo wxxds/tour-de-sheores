b0cc108f4cc116133e5c775a6caebadfea576a2a
Andrew Chase <andrew.chase@accenturefederal.com>
2020-08-25 11:13:08 -0400

    added material ui

--- tour-de-sheroes/src/app/app.component.ts
+++ tour-de-sheroes/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {CommentNode} from './comments/comment-tree.component'
 
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
 })
 
 export class AppComponent {
--- tour-de-sheroes/src/app/dashboard/dashboard.component.ts
+++ tour-de-sheroes/src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
